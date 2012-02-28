98719d4ad1d946dc4c71b6e0a4eaca7a2cec4ec1
Daniel Wertheim <daniel@wertheim.se>
2012-02-28 23:13:12 +0100

    Added Specifications and UnitTests project.

---
+++ Source/Tests/LiteCqrs.Specifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LiteCqrs.Specifications")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cde781a4-55da-4759-aad5-36aac571f684")]
---
+++ Source/Tests/LiteCqrs.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LiteCqrs.UnitTests")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d92b7c81-c8be-4d98-ba4b-dbbf39ba0178")]
