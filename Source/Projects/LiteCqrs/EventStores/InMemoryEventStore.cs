d715c24bff3bde41e62117138565c6c22a7f6c75
Daniel Wertheim <daniel@wertheim.se>
2012-03-18 21:15:21 +0100

    CommittedEvent is only for InMemoryEventStore.

--- Source/Projects/LiteCqrs/EventStores/InMemoryEventStore.cs
+++ Source/Projects/LiteCqrs/EventStores/InMemoryEventStore.cs
@@ -3,34 +3,48 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using LiteCqrs.Eventing;
+using NCore;
 
 namespace LiteCqrs.EventStores
 {
     public class InMemoryEventStore : IEventStore
     {
-        private readonly ConcurrentQueue<ICommittedEvent> _store;
+        private readonly ConcurrentQueue<CommittedEvent> _store;
 
         public InMemoryEventStore()
         {
-            _store = new ConcurrentQueue<ICommittedEvent>();
+            _store = new ConcurrentQueue<CommittedEvent>();
         }
 
         public void Store(IEnumerable<IEvent> events)
         {
             var commitId = Guid.NewGuid();
+            var ts = SysDateTime.Now;
 
             foreach (var e in events)
-                _store.Enqueue(new CommittedEvent { CommitId = commitId, Event = e });
+                _store.Enqueue(new CommittedEvent { CommitId = commitId, TimeStamp = ts, Event = e });
         }
 
         public IEnumerable<IEvent> GetById(Guid aggregateRootId)
         {
-            return _store.Where(e => e.AggregateRootId == aggregateRootId);
+            return _store.Where(e => e.AggregateRootId == aggregateRootId).Select(e => e.Event);
         }
 
         public IEnumerable<IEvent> GetByCommitId(Guid commitId)
         {
-            return _store.Where(ce => ce.CommitId == commitId);
+            return _store.Where(ce => ce.CommitId == commitId).Select(e => e.Event);
+        }
+
+        [Serializable]
+        private class CommittedEvent
+        {
+            public Guid AggregateRootId
+            {
+                get { return Event.AggregateRootId; }
+            }
+            public Guid CommitId { get; set; }
+            public DateTime TimeStamp { get; set; }
+            public IEvent Event { get; set; }
         }
     }
 }
